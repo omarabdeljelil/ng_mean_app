dd35bf32c9fe9c65e0f0ba0196eaf3a6e898184e
Omar Abdeljelil <omarabdeljelil@gmail.com>
2019-05-26 13:52:00 -0400

    adding the backend (nodeJS) + express.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule, MatExpansionModule} from '@angular/material';
 import {HeaderComponent} from './header/header.component';
 import {PostListComponent} from './posts/post-list/post-list.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -26,7 +27,8 @@ import {PostListComponent} from './posts/post-list/post-list.component';
         MatCardModule,
         MatButtonModule,
         MatToolbarModule,
-        MatExpansionModule
+        MatExpansionModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
